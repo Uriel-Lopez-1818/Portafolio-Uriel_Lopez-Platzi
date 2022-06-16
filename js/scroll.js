f30817bc11d613bea074bd934cc7592bacaf655c
Uriel-Lopez-1818 <uriellopezpacheco7805@gmail.com>
2022-06-16 12:36:36 -0500

    agregue funciones en JS para el header

--- js/scroll.js
+++ js/scroll.js
@@ -1,2 +1,13 @@
+const header = document.getElementById("main-header")
+console.log(header)
 
-alert("si jala xD");
+function scrollHeeaderTop () {
+    if(window.scrollY >  500  ) {
+        header.classList.add("fixed-header");
+    } else if (window.scrollY === 0) {
+        header.classList.remove("fixed-header")
+    }
+}
+
+document.addEventListener("scroll", scrollHeeaderTop );
+header.addEventListener("click", scrollHeeaderTop );
